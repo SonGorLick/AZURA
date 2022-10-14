ab2064dd9a0653e5dae4d505c82e5327b251cde5
Synthetic <106828424+Syntheticc@users.noreply.github.com>
2022-10-14 08:55:20 -0400

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -11,7 +11,7 @@ pip install colorama
 pip install time
 pip install webbrowser
 pip install subprocess
-pip install request91
+pip install request2022
 pip install base64
 pip install re
 pip install b64encode
